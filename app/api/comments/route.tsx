ba3bb0281c38764adb7813bc8e9bc45088b43854
swimmingcircle119 <lampmaa22@gmail.com>
2024-03-18 20:53:21 -0700

    post request example

--- app/api/comments/route.tsx
+++ app/api/comments/route.tsx
@@ -4,3 +4,16 @@ export async function GET() {
   // Send a JSON response with the sample data
   return Response.json(comments);
 }
+
+export async function POST(request: Request) {
+  const incomingComment = await request.json();
+  const newComment = {
+    id: 15,
+    ...incomingComment,
+  };
+  comments.push(newComment);
+  return new Response(JSON.stringify(newComment), {
+    headers: { "Content-Type": "application/json" },
+    status: 201,
+  });
+}
