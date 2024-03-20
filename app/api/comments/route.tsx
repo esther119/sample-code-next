efbea87317c6d651e6c88926cb928d0e49df35bd
swimmingcircle119 <lampmaa22@gmail.com>
2024-03-20 16:56:10 -0700

    route handler POST

--- app/api/comments/route.tsx
+++ app/api/comments/route.tsx
@@ -1,18 +1,34 @@
 import { comments } from "./data";
+import { NextResponse } from "next/server";
 
 export async function GET() {
   // Send a JSON response with the sample data
   return Response.json(comments);
 }
 
-export async function POST(request: Request) {
-  const incomingComment = await request.json();
+// Sol 1
+// export async function POST(request: Request) {
+//   const incomingComment = await request.json(); //syntax importance
+//   const newComment = {
+//     id: 15,
+//     ...incomingComment,
+//   };
+//   comments.push(newComment);
+//   return new Response(JSON.stringify(newComment), {
+//     headers: { "Content-Type": "application/json" },
+//     status: 201,
+//   });
+// }
+
+// Sol 2
+export async function POST(request: NextResponse) {
+  const incomingComment = await request.json(); //syntax importance
   const newComment = {
     id: 15,
     ...incomingComment,
   };
   comments.push(newComment);
-  return new Response(JSON.stringify(newComment), {
+  return new NextResponse(JSON.stringify(newComment), {
     headers: { "Content-Type": "application/json" },
     status: 201,
   });
